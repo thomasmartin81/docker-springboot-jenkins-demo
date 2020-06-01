922c318a639b2a2a268ada33120670420044d856
Thomas Martin <thomas.martin@bs.ch>
2020-06-01 11:19:11 +0200

    Changed timezone and validate start class in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ HEALTHCHECK --start-period=10s --interval=5s --timeout=2s --retries=3 \
             CMD curl --silent --fail --request GET http://localhost:8080/actuator/health || exit 1
 
 RUN apt-get update && apt-get -y install curl
+RUN unlink /etc/localtime && ln -s /usr/share/zoneinfo/Europe/Zurich /etc/localtime
 
 # command to execute when image is running
 CMD ["java", "-cp", "/app/libs/*:/app/classes/", "ch.duerri.dockerspringbootdemo.DockerSpringbootDemoApplication"]
--- src/test/java/ch/duerri/dockerspringbootdemo/DockerSpringbootDemoApplicationTests.java
+++ src/test/java/ch/duerri/dockerspringbootdemo/DockerSpringbootDemoApplicationTests.java
@@ -2,14 +2,41 @@ package ch.duerri.dockerspringbootdemo;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @Tag("local")
 @SpringBootTest
 class DockerSpringbootDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void validateSpringBootApplicationClassInDockerfile() {
+        assertTrue(DockerSpringbootDemoApplication.class.isAnnotationPresent(SpringBootApplication.class));
+        File dockerfile = new File("Dockerfile");
+        assertTrue(dockerfile.exists());
+
+        AtomicBoolean found = new AtomicBoolean(false);
+        try (Stream<String> stream = Files.lines(dockerfile.toPath())) {
+            stream.forEach(line -> {
+                if (line.startsWith("CMD") && line.contains(DockerSpringbootDemoApplication.class.getName())) {
+                    assertFalse(found.getAndSet(true));
+                }
+            });
+        } catch (IOException e) {
+            fail(e);
+        }
+        assertTrue(found.get());
+    }
 }
